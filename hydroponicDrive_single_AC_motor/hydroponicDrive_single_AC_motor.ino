7eb2b98095b84c7e1d08bd11a50c2fa3f71bced1
Jordan Tryon <oswegotryon@gmail.com>
2017-11-25 22:08:16 -0500

    Right and Left Side Sync
    Modifications made in order to stop machine from running if the right and left sides desync.

--- hydroponicDrive_single_AC_motor/hydroponicDrive_single_AC_motor.ino
+++ hydroponicDrive_single_AC_motor/hydroponicDrive_single_AC_motor.ino
@@ -1,109 +1,188 @@
-//outlet pin definitions
-#define outlet1 53  //
-#define outlet2 51  //warning lamp for low nutrient tank
+//Declare var names for the pins/outlets
+#define outlet1 53
+#define outlet2 51  //Warning lamp
 #define outlet3 49  //drive motor
 #define outlet4 47  //water pump
 
-//input button definitions
-#define toggleSwitch 41     //switch to toggle between auto and operator control
-#define forwardBtn 43       //button to manually move belt forwards in operator control
-#define backwardBtn 45      //button to manually move belt backwards in operator control
-#define trayLimitSwitch 39  //limit switch to detect incoming tray for watering
-#define waterLevelSwitch 38 //switch to detect water level
+//Inputs
+#define waterSwitch 39
+#define leftSwitch 41
+#define rightSwitch 43
 
-//variables
-int prevTime = millis();  //variable to keep track of the previous trays time
-
-//booleans for outlet on and off states
+//Booleans for outlet on and off states
 bool on = LOW;
 bool off = HIGH;
 
+bool leftSwitchLockout = false;
+bool rightSwitchLockout = false;
+
+int printDelay = 500;
+int printPrevTime = millis();
+int printCount = 0;
+
+int debounceTime = 50;
+int leftSwitchTime = millis();
+int rightSwitchTime = millis();
+int leftSwitchCount = 0;
+int rightSwitchCount = 0;
+
 void setup(){
-  //define outlet pins as outputs
+  //Open the serial port at 9600 bps:
+  Serial.begin(9600);
+  
+  //Define outlet pins as outputs
   pinMode(outlet1,OUTPUT);
   pinMode(outlet2,OUTPUT);
   pinMode(outlet3,OUTPUT);
   pinMode(outlet4,OUTPUT);
-  
-  //set all outlets to off
+
+  //Set all outlets to off
   digitalWrite(outlet1,off);
   digitalWrite(outlet2,off);
   digitalWrite(outlet3,off);
   digitalWrite(outlet4,off);
 
-  //input buttons
-  pinMode(toggleSwitch,INPUT);
-  pinMode(forwardBtn,INPUT);
-  pinMode(backwardBtn,INPUT);
-  
-  //switches
-  pinMode(trayLimitSwitch,INPUT);
-  pinMode(waterLevelSwitch,INPUT);
+  //Limit switches
+  pinMode(waterSwitch,INPUT);
+  pinMode(leftSwitch,INPUT);
+  pinMode(rightSwitch,INPUT);
+
+  Serial.println("Clear Buffer");
+  Serial.println("Starting...");
 }
 
 void loop(){
-  //check the nutrient tank level
-  if(digitalRead(waterLevelSwitch) == LOW){ //if level is low
-    digitalWrite(outlet2,on);               //turn on lamp
-  }else{                                    //when level is back up
-    digitalWrite(outlet2,off);              //turn off lamp
+  /*
+   * Watering routine
+   */
+  //Forward
+  digitalWrite(outlet3,on);
+  
+  if(digitalRead(waterSwitch) == HIGH){
+    //Stop
+    delay(100);
+    digitalWrite(outlet3,off);
+    
+    //Wait for swing to stop
+    delay(300);
+    
+    //Turn on pump till full (time)
+    digitalWrite(outlet4,on);
+    delay(14000);
+    
+    //Turn pump off
+    digitalWrite(outlet4,off);
+    
+    //Wait for dripping to stop (time)
+    delay(3000);
+    
+    //Drive forward until microSwitch is no longer pressed
+    while(digitalRead(waterSwitch) == HIGH){
+      digitalWrite(outlet3,on);
+    }
+  
+    //delay to move away from the switch
+    delay(800);
+      
+    //Turn off drive motor
+    digitalWrite(outlet3,off);
   }
 
-  //check state of the auto/manual toggle switch
-  if(digitalRead(toggleSwitch) == HIGH){  //auto mode
-    if(millis()-prevTime < 90000){  //if time since last tray < 90 secs
-      //turn on drive motor
-      digitalWrite(outlet3,HIGH);
+  /*
+   * Check Sync
+   */
   
-      //wait until switch is pressed
-      while(digitalRead(trayLimitSwitch) == LOW){}
-  
-      //stop drive belt
-      digitalWrite(outlet3,off);
-  
-      //wait for swing to stop
+  //Left Switch
+  if(leftSwitchLockout == false && digitalRead(leftSwitch) == 1){
+    leftSwitchTime = millis();
+    leftSwitchCount++;
+    delay(debounceTime);
+    leftSwitchLockout = true;
+  }
+  if(leftSwitchLockout == true && digitalRead(leftSwitch) == 0){
+    leftSwitchLockout = false;
+    delay(debounceTime);
+  }
+
+  //Right Switch
+  if(rightSwitchLockout == false && digitalRead(rightSwitch) == 1){
+    rightSwitchTime = millis();
+    rightSwitchCount++;
+    delay(debounceTime);
+    rightSwitchLockout = true;
+  }
+  if(rightSwitchLockout == true && digitalRead(rightSwitch) == 0){
+    rightSwitchLockout = false;
+    delay(debounceTime);
+  }
+
+  //Print info
+  int timeDiff = millis() - printPrevTime;
+  if(timeDiff >= printDelay){
+    if(printCount++ % 10 == 0){
+      Serial.println("Time between trays\tLeft count\tRight count");
+    }
+    Serial.print(abs(leftSwitchTime - rightSwitchTime));
+    Serial.print(" ms\t\t\t");
+    Serial.print(leftSwitchCount);
+    Serial.print("\t\t");
+    Serial.print(rightSwitchCount);
+    Serial.println();
+
+    printPrevTime = millis();
+  }
+
+  /*
+   * Stop Conditions
+   */
+
+  //Time between trays exceeded 1000 ms
+  if(leftSwitchCount == rightSwitchCount && abs(leftSwitchTime - rightSwitchTime) > 1000){
+    //!!! STOP !!!
+    //Set all outlets to off
+    digitalWrite(outlet1,off);
+    digitalWrite(outlet2,off);
+    digitalWrite(outlet3,off);
+    digitalWrite(outlet4,off);
+    
+    Serial.println("Time between trays\tLeft count\tRight count");
+    Serial.print(abs(leftSwitchTime - rightSwitchTime));
+    Serial.print(" ms\t\t\t");
+    Serial.print(leftSwitchCount);
+    Serial.print("\t\t");
+    Serial.print(rightSwitchCount);
+    Serial.println();
+
+    while(true){
+      digitalWrite(outlet2,on);
+      delay(1000);
+      digitalWrite(outlet2,off);
+      delay(1000);
+    }
+  }
+
+  //Tray count differed by 2 or more
+  if(abs(leftSwitchCount - rightSwitchCount) >= 2){
+    //!!! STOP !!!
+    //Set all outlets to off
+    digitalWrite(outlet1,off);
+    digitalWrite(outlet2,off);
+    digitalWrite(outlet3,off);
+    digitalWrite(outlet4,off);
+    
+    Serial.println("Time between trays\tLeft count\tRight count");
+    Serial.print(abs(leftSwitchTime - rightSwitchTime));
+    Serial.print(" ms\t\t\t");
+    Serial.print(leftSwitchCount);
+    Serial.print("\t\t");
+    Serial.print(rightSwitchCount);
+    Serial.println();
+
+    while(true){
+      digitalWrite(outlet2,on);
       delay(500);
-  
-      //turn on pump until full (time)
-      digitalWrite(outlet4,on);
-      delay(6000);
-  
-      //turn off pump
-      digitalWrite(outlet4,off);
-  
-      //wait for driping to stop
-      delay(2000);
-  
-      //drive forward
-      digitalWrite(outlet3,on);
-      
-      //wait until limit switch is no longer pressed
-      while(digitalRead(trayLimitSwitch) == HIGH){}
-      
-      //delay to move away from the switch
-      delay(400);
-      
-      //Turn off drive motor;
-      digitalWrite(outlet3,off);
-      
-      //reset previous time
-      prevTime = millis();
-    }else{  //if time since last tray >= 90 secs
-      digitalWrite(outlet3,off);  //turn off drive belt
-      digitalWrite(outlet4,off);  //turn off pump
-      while(true){}
+      digitalWrite(outlet2,off);
+      delay(500);
     }
-  }else{  //manual mode
-    //turn off water pump
-    digitalWrite(outlet4,LOW);
-    
-    if(digitalRead(forwardBtn) == HIGH){  //if forward button is pressed
-      digitalWrite(outlet3,HIGH);         //drive belt forward
-    }else{                                //otherwise
-      digitalWrite(outlet3,LOW);          //stop belt
-    }
-    
-    //when exiting manual mode, reset time since last tray
-    prevTime = millis();
   }
 }
