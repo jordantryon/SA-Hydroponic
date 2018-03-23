eff351c8a0e37a5192e240d1ad8a6577d2618ec5
Jordan Tryon <oswegotryon@gmail.com>
2018-03-23 19:36:52 -0400

    Optimization
    Optimized new revision of code, also  removed time error checking.

--- verticalFarmArduinoNew/verticalFarmArduinoNew.ino
+++ verticalFarmArduinoNew/verticalFarmArduinoNew.ino
@@ -1,32 +1,31 @@
-//Temperature libraries
+//temperature libraries
 #include <OneWire.h>
 #include <DallasTemperature.h>
-//Humidity libraries
-#include "DHT.h"
+//humidity libraries
+#include <DHT.h>
+//LCD library
+#include <LiquidCrystal.h>
 
-//Declare var names for the outlets
+//declare var names for the outlets
 #define outlet1 53  //secondary refill pump
 #define outlet2 51  //warning lamp
 #define outlet3 49  //drive motor
 #define outlet4 47  //water pump
 
-//Inputs
-#define waterSwitch 39
-#define leftSwitch 41
-#define rightSwitch 43
+//inputs
 #define eStop 45
+#define rightSwitch 43
+#define leftSwitch 41
+#define waterSwitch 39
 #define waterLevelSwitch 37
 
-//Booleans for outlet on and off states
+//booleans for outlet on and off states
 bool on = LOW;
 bool off = HIGH;
 
+//logic for count sync
 bool leftSwitchLockout = false;
 bool rightSwitchLockout = false;
-
-int printDelay = 500;
-int printPrevTime = millis();
-int printCount = 0;
 
 int debounceTime = 100;
 int leftSwitchTime = millis();
@@ -34,73 +33,84 @@ int rightSwitchTime = millis();
 int leftSwitchCount = 0;
 int rightSwitchCount = 0;
 
-//Temperature Sensors
+//temperature Sensors
 #define ONE_WIRE_BUS 8
-OneWire oneWire(ONE_WIRE_BUS);  //Setup a oneWire instance to communicate with oneWire devices
-DallasTemperature sensors(&oneWire);  //Pass the oneWire reference to Dallas Temperature
+OneWire oneWire(ONE_WIRE_BUS);  //setup a oneWire instance to communicate with oneWire devices
+DallasTemperature sensors(&oneWire);  //pass the oneWire reference to Dallas Temperature
 
-//Humidity Sensor
+//humidity Sensor
 #define DHTPIN 9
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
+//declare sensor value variables
+double tempValA;
+double tempValW;
+double humidVal;
+
+//initialize the LCD with pins
+#define RS 12
+#define EN 11
+#define D4 5
+#define D5 4
+#define D6 3
+#define D7 2
+LiquidCrystal lcd(RS,EN,D4,D5,D6,D7);
+
 void setup(){
-  //Open the serial port at 9600 baud
+  //open the serial port at 9600 baud
   Serial.begin(9600);
 
-  //Define outlet pins as outputs
+  //define outlet pins as outputs
   pinMode(outlet1,OUTPUT);
   pinMode(outlet2,OUTPUT);
   pinMode(outlet3,OUTPUT);
   pinMode(outlet4,OUTPUT);
 
-  //Set all outlets to off
-  digitalWrite(outlet1,off);
-  digitalWrite(outlet2,off);
-  digitalWrite(outlet3,off);
-  digitalWrite(outlet4,off);
+  outletsOff();
 
-  //Limit switches
+  //limit switches
   pinMode(waterSwitch,INPUT);
   pinMode(leftSwitch,INPUT);
   pinMode(rightSwitch,INPUT);
 
-  //Switches
+  //switches
   pinMode(eStop,INPUT);
   pinMode(waterLevelSwitch,INPUT);
 
-  //Start the oneWire library
+  //start the oneWire library
   sensors.begin();
 
-  //Humidity Sensor
+  //humidity Sensor
   dht.begin();
+
+  //LCD
+  lcd.begin(20,4);
 
   Serial.println("Clear Buffer");
   Serial.println("Starting...");
 }
-
-double tempValA;
-double tempValW;
-double humidVal;
 
 void loop(){
   /*
    * Watering routine
    */
 
-  //Forward
+  //forward
   digitalWrite(outlet3,on);
 
   if(digitalRead(waterSwitch) == HIGH){
-    //Stop
-    delay(100);
+    //stop
+    delay(250);
     digitalWrite(outlet3,off);
 
-    //Turn on pump till full (time)
+    //turn pump on
     digitalWrite(outlet4,on);
+
+    //wait for pumping duration
     delay(14000);
 
-    //Turn pump off
+    //turn pump off
     digitalWrite(outlet4,off);
 
     /*
@@ -124,12 +134,11 @@ void loop(){
     String s6 = "," + (String)leftSwitchCount;
     Serial.println(s1 + s2 + s3 + s4 + s5 + s6);
 
-    //Drive forward until microSwitch is no longer pressed
-    while(digitalRead(waterSwitch) == HIGH){
-      digitalWrite(outlet3,on);
-    }
+    //drive forward until microSwitch is no longer pressed
+    digitalWrite(outlet3,on);
+    while(digitalRead(waterSwitch) == HIGH){}
 
-    //delay to move away from the switch
+    //delay to move away from the switch, prevent double presses
     delay(200);
   }
 
@@ -137,7 +146,7 @@ void loop(){
    * Check Sync
    */
 
-  //Left Switch
+  //left switch
   if(leftSwitchLockout == false && digitalRead(leftSwitch) == 1){
     leftSwitchTime = millis();
     leftSwitchCount++;
@@ -149,7 +158,7 @@ void loop(){
     delay(debounceTime);
   }
 
-  //Right Switch
+  //right switch
   if(rightSwitchLockout == false && digitalRead(rightSwitch) == 1){
     rightSwitchTime = millis();
     rightSwitchCount++;
@@ -165,61 +174,26 @@ void loop(){
    * Stop Conditions
    */
 
-  //Time between trays exceeded 2000 ms
-  if(leftSwitchCount == rightSwitchCount && abs(leftSwitchTime - rightSwitchTime) > 2000){
-    //!!! STOP !!!
-    //Set all outlets to off
-    digitalWrite(outlet1,off);
-    digitalWrite(outlet2,off);
-    digitalWrite(outlet3,off);
-    digitalWrite(outlet4,off);
-
-    Serial.println("ERROR: time");
-
-    while(true){
-      digitalWrite(outlet2,on);
-      delay(1000);
-      digitalWrite(outlet2,off);
-      delay(1000);
-    }
-  }
-
-  //Tray count differed by 2 or more
+  //tray count differs by 2 or more
   if(abs(leftSwitchCount - rightSwitchCount) >= 2){
-    //!!! STOP !!!
-    //Set all outlets to off
-    digitalWrite(outlet1,off);
-    digitalWrite(outlet2,off);
-    digitalWrite(outlet3,off);
-    digitalWrite(outlet4,off);
+    outletsOff();
 
     Serial.println("ERROR: count");
 
     while(true){
-      digitalWrite(outlet2,on);
-      delay(500);
-      digitalWrite(outlet2,off);
-      delay(500);
+      blinkLight(500);
     }
   }
 
-  //eStop was pressed
+  //eStop is pressed
   if(digitalRead(eStop) == LOW){
-    //!!! STOP !!!
-    //Set all outlets to off
-    digitalWrite(outlet1,off);
-    digitalWrite(outlet2,off);
-    digitalWrite(outlet3,off);
-    digitalWrite(outlet4,off);
+    outletsOff();
 
     Serial.println("ERROR: eStop");
     delay(250);
 
     while(digitalRead(eStop) == LOW){
-      digitalWrite(outlet2,on);
-      delay(200);
-      digitalWrite(outlet2,off);
-      delay(200);
+      blinkLight(200);
     }
     delay(250);
   }
@@ -227,12 +201,7 @@ void loop(){
   //water level too low, switch is high
   //NOTE: stops system to refill res then resumes
   if(digitalRead(waterLevelSwitch) == HIGH){
-    //!!! STOP !!!
-    //Set all outlets to off
-    digitalWrite(outlet1,off);
-    digitalWrite(outlet2,off);
-    digitalWrite(outlet3,off);
-    digitalWrite(outlet4,off);
+    outletsOff();
 
     Serial.println("ERROR: water");
 
@@ -243,3 +212,44 @@ void loop(){
     digitalWrite(outlet1,off);
   }
 }
+
+/*
+ * Functions
+ */
+
+void blinkLight(int d){
+  digitalWrite(outlet2,on);
+  delay(d);
+  digitalWrite(outlet2,off);
+  delay(d);
+}
+
+void outletsOff(){
+  digitalWrite(outlet1,off);
+  digitalWrite(outlet2,off);
+  digitalWrite(outlet3,off);
+  digitalWrite(outlet4,off);
+}
+
+void updateLCD(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Ta: " + (String)tempValA);
+  lcd.setCursor(10,0);
+  lcd.print("Tw: " + (String)tempValW);
+  lcd.setCursor(0,1);
+  lcd.print("H%: " + (String)humidVal);
+}
+
+/*
+//time between trays exceeded 2000 ms
+if(leftSwitchCount == rightSwitchCount && abs(leftSwitchTime - rightSwitchTime) > 2000){
+  outletsOff();
+
+  Serial.println("ERROR: time");
+
+  while(true){
+    blinkLight(1000);
+  }
+}
+*/
